913f582008fcfcbca21525229e34ac3789a32b71
Andrew Dunstan <andrewdunstan@Andrews-MacBook-Pro.local>
2023-08-12 23:16:12 -0400

    Added boost unit tests to the project

--- tests/util_tests.cpp
+++ tests/util_tests.cpp
@@ -5,7 +5,13 @@
 
 BOOST_AUTO_TEST_CASE(first_test) 
 {
-  int i = 1;
-  BOOST_TEST(i); 
-  BOOST_TEST(i == 2); 
+    int i = 1;
+    BOOST_TEST(i); 
+    BOOST_TEST(i == 1); 
+}
+
+BOOST_AUTO_TEST_CASE(test_add_two_numbers) 
+{
+    int i = add_two_numbers(3, -2);
+    BOOST_TEST(i == 1); 
 }

506409244d19411d0d740cf3714b70a2ee2b70f1
Andrew Dunstan <andrewdunstan@Andrews-MacBook-Pro.local>
2023-08-12 21:36:49 -0400

    Added a version number

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+// Standard Includes
 #include <iostream>
 
+// Local Includes
+#include "ChessConfig.h"
+
 int main(){
-    std::cout << "hello world\n";
+    std::cout << "Welcome to Andrew's chess project!\n";
+    std::cout << "Version " << Chess_VERSION_MAJOR << "." << Chess_VERSION_MINOR << "\n";
     return 0;
 }
